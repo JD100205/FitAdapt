d42e3a923ef9fdd9e26004c98f2fde144a8c173e
JD1002 <juangallegos1002@gmail.com>
2026-05-02 07:36:51 -0500

    Feat: Inicialización del esqueleto de FITADAPT con Spring Boot

---
+++ fitadapt/src/main/java/com/fitadapt/FitadaptApplication.java
@@ -0,0 +1,13 @@
+package com.fitadapt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitadaptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitadaptApplication.class, args);
+	}
+
+}
